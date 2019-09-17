59c7261276a8740ed765abe3e4a38f3b0735ce09
Maks_Bubela <ya.bubela@gmail.com>
2019-09-17 19:43:14 +0300

    lol prekol

---
+++ Lesson_1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,sum;
+    cout <<"Insert the number : ";
+    cin >> a;
+    cout <<"Insert the number : ";
+    cin >> b;
+    sum = a+b;
+    cout <<"The result is : " << sum;
+}
