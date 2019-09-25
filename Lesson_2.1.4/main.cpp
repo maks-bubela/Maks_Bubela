a82a4cab0e878b62458919855251bd8738c56d24
Maks_Bubela <ya.bubela@gmail.com>
2019-09-25 17:21:11 +0300

    elachkaaaaaaaaaaaaaaaaaaaaaa

--- Lesson_2.1.4/main.cpp
+++ Lesson_2.1.4/main.cpp
@@ -7,10 +7,17 @@ int main()
     int a;
     cout << "Christmas tree size : ";
     cin >> a;
-    for (int i=0;i<=a;i++){
-        for (int q=i;q!=0;q--){
+    for (int i=1;i<a;i+=2){
+        for (int q=0;q<a-(i/2);q++){
+            cout << " ";
+        }
+        for (int q=0;q<i;q++){
             cout << "*";
         }
         cout << endl;
     }
+        for (int q=0;q<a;q++){
+            cout << " ";
+        }
+            cout <<"*";
 }
