7d4cd116eb1192a9656c79b68d2bdc88d5e519d9
Maks_Bubela <ya.bubela@gmail.com>
2019-10-15 00:23:18 +0300

    fixed problem with word count

--- Lesson_3/main.cpp
+++ Lesson_3/main.cpp
@@ -5,33 +5,81 @@ using namespace std;
 
 int main()
 {
-    char karta[25][50];
-    int sz1=25,sz2=50;
-    int x,y;
-    srand(time(0));
-    x=1+ rand()%49;
-    y=1+ rand()%24;
-    cout << x << " " << y << endl;
+    char pmap[25][50]; //Massive for map
+    int pposx,pposy; // coordinates of user character
+    int newposx,newposy; // new coordinates of user character
+    int sz1=25,sz2=50; // size counter for map
 
+
+    srand(time(0));
+    pposx=1+ rand()%49;                 // here we make random x and y for user character
+    pposy=1+ rand()%24;
+
+    newposx=pposx;
+    newposy=pposy;
 
     for (int i=0;i<sz1;i++){
         for (int q=0;q<sz2;q++){
-            if (q==0||q==sz2-1||i==0||i==sz1-1){
-            karta[i][q]='@';
+            if (q==0
+                ||q==sz2-1
+                ||i==0
+                ||i==sz1-1){
+            pmap[i][q]='@';
             }
-            else if (q==x&&i==y){
-                karta[i][q]='T';
+            else if (q==pposx && i==pposy){                       // in this cycle we make map, the borders of map and user character
+                pmap[i][q]='T';
             }
             else {
-                karta[i][q]=' ';
+                pmap[i][q]=' ';
             }
         }
     }
-    for (int i=0;i<sz1;i++){
-        for (int q=0;q<sz2;q++){
-            cout << karta[i][q];
-        }
+    while(1){
+        system("cls");              //clear screen
+        for (int i=0;i<sz1;i++){
+            for (int q=0;q<sz2;q++){            // in this cycle we outputing the map
+                cout << pmap[i][q];
+            }
         cout << endl;
+        }
+        int action;
+        cout << "\n\n > ";
+        cin >> action;               // here we ask user about what he want to do
+        newposx=pposx;
+        newposy=pposy;
+
+        switch (action) {
+            case 8:
+                newposy++;
+                break;;
+            case 2:
+                newposy--;                      // in this switch we check what character must to do
+                break;
+            case 4:
+                newposx--;
+                break;
+            case 6:
+                newposx++;
+                break;
+            case 0:
+                return(0);
+        }
+        if(newposx==0){
+            newposx=sz1-1;
+            pposx=newposx;
+        }
+        else if(newposy==0){
+            newposy=sz2-1;
+            pposy=newposy;
+        }
+        else if(newposx==sz1){
+            newposx=sz1-(sz1-1);
+            pposx=newposx;
+        }
+        else if(newposy==sz2){
+            newposy=sz2-(sz2-1);
+            pposy=newposy;
+        }
+
     }
-    return 0;
 }
--- Lesson_4.1.1/main.cpp
+++ Lesson_4.1.1/main.cpp
@@ -5,16 +5,56 @@ using namespace std;
 int main()
 {
     char slovo[100]; // massive for words in lines
-    int i=0,q=1; // increments
+    int i=0,b=0,k=1; // increments
+    char letters[64];  // massive for letters
+    int uppercase=65,lowercase=97; // variable for value letters in ascii code
+    for (int i=0;i<=64;i++){
+        if (i<=32){
+            letters[i]=uppercase;               // cycle give to massive letters value letters in ascii code
+            uppercase++;
+        }
+        if (i>32){
+            letters[i]=lowercase;
+            lowercase++;
+        }
+    }
     for (int i=0;i<100;i++)
         slovo[i]=0;
     cout << "Enter your line here (max 100 words) : ";
     cin.getline(slovo,100);
     i=1;
     while (slovo[i]!=0){
-        if (slovo[i] ==' '&& slovo[i-1]!=' ')
-            q++;
+        for (int q=0;q<=64;q++){
+            if (slovo[i]==letters[q]){              // here we check whether this variable is a letter
+                b=1;
+                break;
+            }
+        }
+        if (b!=1){
+            for (int q=0;q<64;q++){
+                if (slovo[i-1]==letters[q]){
+                    int a=i;
+                    while (slovo[a]!=0){
+                        for (int j=0;j<64;j++){
+                            if (slovo[a]==letters[j]){                  // here we check how much words in a line
+                                b=1;
+                                break;
+                            }
+                            else
+                                b=0;
+                        }
+                        if (b==1){
+                            k++;
+                            break;
+                        }
+                        a++;
+                    }
+                }
+
+            }
+        }
+        b=0;
         i++;
     }
-    cout << "The number of words in your line : " << q ;
+    cout << "The number of words in your line : " << k ;
 }
