15590b43cfd8c3d970f2a004e7b24226691ec759
Maks_Bubela <ya.bubela@gmail.com>
2019-10-16 21:17:36 +0300

    change from numbers to letters and deleted massive

--- Lesson_4.1.2/main.cpp
+++ Lesson_4.1.2/main.cpp
@@ -8,11 +8,6 @@ int main()
     int i=0,k=0; // increments
     int lnumb=0; // The largest number
     int raz=0; // difference between increment i and largest number
-    int cletter[4]; // ascii counter for letters
-    cletter[0]=65;
-    cletter[1]=90;
-    cletter[2]=97;
-    cletter[3]=122;
     for (int i=0;i<100;i++)
         slovo[i]=0;
     cout << "Enter your line here (max 100 words) : ";
@@ -20,8 +15,8 @@ int main()
     i=0;
     while (slovo[i]!=0){
 
-        if ((slovo[i]>=cletter[0] && slovo[i]<=cletter[1])||
-            (slovo[i]>=cletter[2] && slovo[i]<=cletter[3])) // here we check whether this variable is a letter
+        if ((slovo[i]>='A' && slovo[i]<='Z')||
+            (slovo[i]>='a' && slovo[i]<='z')) // here we check whether this variable is a letter
             k++;
         else {
             if (k>lnumb){                       //here we check the largest word with other words
