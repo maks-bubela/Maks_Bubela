4b11422b2d8e3978d8de4d47de2ca5a3d990111e
Maks_Bubela <ya.bubela@gmail.com>
2019-10-17 21:45:19 +0300

    DEleted while and added if

--- Lesson_1.1.2/main.cpp
+++ Lesson_1.1.2/main.cpp
@@ -11,19 +11,10 @@ int main()
         cout << "Work more!!!" << endl;
         goto exit;
     }
-
-        if (a<10000000){
-            while (a<1000000){
-                cout << "Good" << endl;
-                goto exit;
-            }
-            while (a>999999){
-                cout << "You are a millionaire" << endl;
-                goto exit;
-            }
-        }
-        cout << "You are a millionaire" <<endl;
-
-    exit:
+    if (a<1000000)
+        cout << "Good" << endl;
+    if (a>999999)
+        cout << "You are a millionaire" << endl;
+        exit :
     cout << "But you done";
-}
+    }
