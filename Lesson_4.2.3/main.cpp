6ebd36f1f199d44260f92d2daac62481c058085a
Maks_Bubela <ya.bubela@gmail.com>
2019-10-21 18:17:19 +0300

    Fixed problem with deliting cymbols

--- Lesson_4.2.3/main.cpp
+++ Lesson_4.2.3/main.cpp
@@ -7,40 +7,46 @@ int main()
     char slovo[100],slovo2[100]; // Massives for words
     for (int i=0;i<100;i++)
         slovo[i]=0;
-    int i=0,q=0,k=0,b=0; // increments
+    int counter1=0,counter2=0,maincount=0,minorcount=0,a=0,usedlet=0; // increments
     cout << "Enter here your 1 line : ";
     cin.getline(slovo,100);
     cout << "Enter here your 2 line : ";
     cin.getline(slovo2,100);
-    while (slovo[i]!=0)
-        i++;
-    while (slovo2[q]!=0)
-        q++;
-    while (slovo[k]!=0){
-        for (int l=0;l<q;l++){
-            for (int b=0;b<i;b++){
+    while (slovo[counter1]!=0)
+        counter1++;
+    while (slovo2[counter2]!=0)
+        counter2++;
+    while (slovo[maincount]!=0){
+        for (int l=usedlet;l<counter2;l++){
+            for (int b=0;b<counter1;b++){
                 if(slovo[b]==slovo2[l]){            //here we deleted trash letters
                     slovo[b]=' ';
+                    a=1;
+                    usedlet++;
+                    break;
                 }
             }
+            if (a==1)
+                break;
         }
-        k++;
+        a=0;
+        maincount++;
     }
 
-    b=1;
-    k=0;
-    while (slovo[k]!=0){
-        while (slovo[k]==' '){
-            if (slovo[k+b]!=0){
-                slovo[k]=slovo[k+b];            // here we deleted gups
-                slovo[k+b]=' ';
-                b++;
+    minorcount=1;
+    maincount=0;
+    while (slovo[maincount]!=0){
+        while (slovo[maincount]==' '){
+            if (slovo[maincount+minorcount]!=0){
+                slovo[maincount]=slovo[maincount+minorcount];            // here we deleted gups
+                slovo[maincount+minorcount]=' ';
+                minorcount++;
             }
             else
                 break;
         }
-        b=1;
-        k++;
+        minorcount=1;
+        maincount++;
     }
     cout << "Your line is : " << slovo;
 }
