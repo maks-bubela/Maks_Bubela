015244ac91a67859ec9a1f945afcf96c4e23cb85
Maks_Bubela <ya.bubela@gmail.com>
2019-10-28 20:00:15 +0300

    Redid the whole programm

--- Lesson_4.1.1/main.cpp
+++ Lesson_4.1.1/main.cpp
@@ -5,59 +5,19 @@ using namespace std;
 int main()
 {
     char slovo[100]; // massive for words in lines
-    int maincount=1,checkw=0,words=0; // increments
-    char letters[64];  // massive for letters
-    int uppercase=65,lowercase=97; // variable for value letters in ascii code
-    for (int i=0;i<='A';i++){
-        if (i<='Z'){
-            letters[i]=uppercase;               // cycle give to massive letters value letters in ascii code
-            uppercase++;
-        }
-        if (i>'Z'){
-            letters[i]=lowercase;
-            lowercase++;
-        }
-    }
+    int maincount=1,words=0; // increments
     for (int i=0;i<100;i++)
         slovo[i]=0;
     cout << "Enter your line here (max 100 words) : ";
     cin.getline(slovo,100);
 
-    while (slovo[maincount]!=0){
-        for (int q=0;q<='A';q++){
-            if (slovo[maincount]==letters[q]){              // here we check whether this variable is a letter
-                checkw=1;
-                break;
-            }
-        }
-        if (checkw!=1){
-            for (int q=0;q<'A';q++){
-                if (slovo[maincount-1]==letters[q]){
-                    int a=maincount;
-                    while (slovo[a]!=0){
-                        for (int j=0;j<'A';j++){
-                            if (slovo[a]==letters[j]){                  // here we check how much words in a line
-                                checkw=1;
-                                break;
-                            }
-                            else
-                                checkw=0;
-                        }
-                        if (checkw==1){
-                            words++;
-                            break;
-                        }
-                        a++;
-                    }
-                }
-            }
-        }
-        else if (words==0 && checkw==1)
-            words++;
-        checkw=0;
+    while(slovo[maincount]!=0){
+        if((slovo[maincount]>='A' && slovo[maincount]<='Z' && ((slovo[maincount-1]>'Z' && slovo[maincount-1]<'a') ||
+           (slovo[maincount-1]<'A' || words==0 ))) ||
+           (slovo[maincount]>='a' && slovo[maincount]<='z' && ((slovo[maincount-1]>'Z' && slovo[maincount-1]<'a') ||
+           (slovo[maincount-1]<'A' || words==0 ))))
+           words++;
         maincount++;
-
-
     }
     cout << "The number of words in your line : " << words ;
 }
