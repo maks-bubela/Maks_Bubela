79e4de343ba963962b5e01033fd21997a34761d8
Maks_Bubela <ya.bubela@gmail.com>
2019-10-28 21:28:57 +0300

    Fixed problem with count words

--- Lesson_4.1.1/main.cpp
+++ Lesson_4.1.1/main.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int main()
 {
     char slovo[100]; // massive for words in lines
-    int maincount=1,words=0; // increments
+    int maincount=0,words=0; // increments
     for (int i=0;i<100;i++)
         slovo[i]=0;
     cout << "Enter your line here (max 100 words) : ";
     cin.getline(slovo,100);
 
     while(slovo[maincount]!=0){
-        if((slovo[maincount]>='A' && slovo[maincount]<='Z' && ((slovo[maincount-1]>'Z' && slovo[maincount-1]<'a') ||
+        if((slovo[maincount]>='A' && slovo[maincount]<='Z' && ((slovo[maincount+1]>'Z' && slovo[maincount+1]<'a') ||
            (slovo[maincount-1]<'A' || words==0 ))) ||
-           (slovo[maincount]>='a' && slovo[maincount]<='z' && ((slovo[maincount-1]>'Z' && slovo[maincount-1]<'a') ||
+           (slovo[maincount]>='a' && slovo[maincount]<='z' && ((slovo[maincount+1]>'Z' && slovo[maincount+1]<'a') ||
            (slovo[maincount-1]<'A' || words==0 ))))
            words++;
         maincount++;
