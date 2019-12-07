5336ff5556a344556fa4b589407a1fa02f40977e
Maks_Bubela <ya.bubela@gmail.com>
2019-12-07 12:56:20 +0300

    Done lesson7.2.1 and added instructions

--- build-Lesson721-Desktop_Qt_5_12_5_MinGW_64_bit-Debug/ui_cryptographer.h
+++ build-Lesson721-Desktop_Qt_5_12_5_MinGW_64_bit-Debug/ui_cryptographer.h
@@ -17,6 +17,7 @@
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -34,6 +35,11 @@ public:
     QLabel *label;
     QLabel *label_2;
     QLineEdit *key;
+    QWidget *widget;
+    QVBoxLayout *verticalLayout;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_5;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -71,6 +77,27 @@ public:
         key = new QLineEdit(centralwidget);
         key->setObjectName(QString::fromUtf8("key"));
         key->setGeometry(QRect(170, 160, 181, 31));
+        widget = new QWidget(centralwidget);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(170, 60, 340, 64));
+        verticalLayout = new QVBoxLayout(widget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        label_3 = new QLabel(widget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+
+        verticalLayout->addWidget(label_3);
+
+        label_4 = new QLabel(widget);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+
+        verticalLayout->addWidget(label_4);
+
+        label_5 = new QLabel(widget);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+
+        verticalLayout->addWidget(label_5);
+
         Cryptographer->setCentralWidget(centralwidget);
         menubar = new QMenuBar(Cryptographer);
         menubar->setObjectName(QString::fromUtf8("menubar"));
@@ -95,6 +122,9 @@ public:
         decrypt->setText(QApplication::translate("Cryptographer", "Decrypt", nullptr));
         label->setText(QApplication::translate("Cryptographer", "Key", nullptr));
         label_2->setText(QApplication::translate("Cryptographer", "Text", nullptr));
+        label_3->setText(QApplication::translate("Cryptographer", "Firstly you must enter text and key", nullptr));
+        label_4->setText(QApplication::translate("Cryptographer", "Secondly you must make choice between crypt and decrypt", nullptr));
+        label_5->setText(QApplication::translate("Cryptographer", "Thirdly you must make choice between cesar and Vigener", nullptr));
     } // retranslateUi
 
 };
